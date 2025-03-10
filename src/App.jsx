760205231c39d42e373b3d2eb190a44c55cc5655
Sresta Bhattarai <SrestaBhattarai@Srestaas-Mac-Air.local>
2025-03-10 06:56:17 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import Cart from './Pages/Cart'
+import LoginSignup from './Pages/LoginSignup'
+import Product from './Pages/Product'
+import Shopcategory from './Pages/Shopcategory'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />}></Route>
+          <Route path='/mens' element={<Shopcategory category='men'/>}></Route>
+          <Route path='/womens' element={<Shopcategory category='women'/>}></Route>
+          <Route path='/kids' element={<Shopcategory category= 'kid' />}></Route>
+          <Route path="/product" element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart />}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
